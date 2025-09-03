16ee6de917ff04751e2f679a0dd086cef5204cf5
Marcos Vogado <marcoshcvogado@gmail.com>
2025-09-03 15:07:13 -0300

    Add ListarPensamentos component and improve form binding
    Introduces the ListarPensamentosComponent with its template, styles, and test file. Updates CriarPensamentoComponent to use ngModel for two-way data binding and adds button click handlers for saving and canceling thoughts. Also imports FormsModule in app.module.ts to support form features.

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -7,11 +7,19 @@ import { Component } from '@angular/core';
 })
 export class CriarPensamentoComponent {
 
+  criarPensamento() {
+    window.alert('Novo pensamento adicionado!')
+  }
+
+  cancelarPensamento() {
+    window.alert('Pensamento cancelado!')
+  }
+
   pensamento = {
     id: '1',
     conteudo: 'Aprendendo Angular',
     autoria: 'Dev',
-    modelo: ''
+    modelo: 'modelo1'
   }
 
 }
