432980b47d3d094436d64cb6dce4b29d8e706e2a
Marcos Vogado <marcoshcvogado@gmail.com>
2025-09-04 15:55:31 -0300

    Add PensamentoComponent and integrate with list view
    Created the PensamentoComponent with its template, styles, and test. Updated ListarPensamentosComponent to render a list of PensamentoComponent instances, passing data via @Input. Registered the new component in AppModule and improved the list template to handle empty states.

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class ListarPensamentosComponent {
 
+  listaPensamentos = [
+    {
+      conteudo: 'passo informações para o componente filho',
+      autoria: 'componente pai',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab minima ipsa sunt veniam, nisi laborum, nobis quos earum iusto praesentium esse ducimus ipsum molestiae, fugiat quod odio magnam libero!Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab minima ipsa sunt veniam, nisi laborum, nobis quos earum iusto praesentium esse ducimus ipsum molestiae, fugiat quod odio magnam libero!',
+      autoria: 'Texto grande',
+      modelo: 'modelo3'
+    } 
+  ];
+
+  constructor(){ }
+
 }
