3ad5d467b896279d030e64767961d0a00d6ca55a
Marcos Vogado <marcoshcvogado@gmail.com>
2025-09-08 14:20:10 -0300

    Add Pensamento service and model, update components
    Introduced PensamentoService and Pensamento interface to improve type safety and enable future service logic. Updated PensamentoComponent and ListarPensamentosComponent to use the Pensamento model. Modified db.json and backend/package.json for backend improvements.

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamentos',
@@ -7,23 +8,7 @@ import { Component } from '@angular/core';
 })
 export class ListarPensamentosComponent {
 
-  listaPensamentos = [
-    {
-      conteudo: 'passo informações para o componente filho',
-      autoria: 'componente pai',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com @input()',
-      autoria: 'Componente filho',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab minima ipsa sunt veniam, nisi laborum, nobis quos earum iusto praesentium esse ducimus ipsum molestiae, fugiat quod odio magnam libero!Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab minima ipsa sunt veniam, nisi laborum, nobis quos earum iusto praesentium esse ducimus ipsum molestiae, fugiat quod odio magnam libero!',
-      autoria: 'Texto grande',
-      modelo: 'modelo3'
-    } 
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   constructor(){ }
 
