eb7790f0ec0c43d99e140d01d8ad5069724dd1f7
Marcos Vogado <marcoshcvogado@gmail.com>
2025-09-15 14:10:37 -0300

    Add validation to editar-pensamento form
    Introduces required and minlength validation for 'conteudo' and 'autoria' fields in the editar-pensamento component. Updates the template to show error messages and disables the save button when the form is invalid. Also improves CSS for error messages and button states.

--- src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
-import { PensamentoService } from '../pensamento.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { PensamentoService } from './../pensamento.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-pensamento',
   templateUrl: './editar-pensamento.component.html',
   styleUrls: ['./editar-pensamento.component.css']
 })
-export class EditarPensamentoComponent {
+export class EditarPensamentoComponent implements OnInit {
 
   formulario!: FormGroup;
 
-  constructor (
+  constructor(
     private service: PensamentoService,
     private router: Router,
     private route: ActivatedRoute,
@@ -23,21 +23,34 @@ export class EditarPensamentoComponent {
     const id = this.route.snapshot.paramMap.get('id')
     this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
       this.formulario = this.formBuilder.group({
-          id: [pensamento.id],
-          conteudo: [pensamento.conteudo],
-          autoria: [pensamento.autoria],
-          modelo: [pensamento.modelo]
-        })
+        id: [pensamento.id],
+        conteudo: [pensamento.conteudo, Validators.compose([
+          Validators.required,
+          Validators.pattern(/(.|\s)*\S(.|\s)*/)
+        ])],
+        autoria: [pensamento.autoria, Validators.compose([
+          Validators.required,
+          Validators.minLength(3)
+        ])],
+        modelo: [pensamento.modelo]
       })
+    })
   }
 
   editarPensamento() {
     this.service.editar(this.formulario.value).subscribe(() => {
       this.router.navigate(['/listarPensamento'])
     })
-  }                                                   
+  }
 
   cancelar() {
     this.router.navigate(['/listarPensamento'])
+  }
+
+  habilitarBotao(): string {
+    if(this.formulario.valid) {
+      return "botao"
+    }
+    else return "botao__desabilitado"
   }
 }
