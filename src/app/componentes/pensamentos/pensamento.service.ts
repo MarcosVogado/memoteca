7284c9c91eaac8c1c84dd6abc297431fd5cbc6af
Marcos Vogado <marcoshcvogado@gmail.com>
2025-09-16 15:03:38 -0300

    Add 'Carregar Mais' button component and update service
    Introduced the BotaoCarregarMaisComponent for loading more items in the pensamentos list and registered it in the app module. Updated PensamentoService to use HttpParams for paginated requests. Also added a new test entry to db.json.

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Pensamento } from './pensamento';
 import { Observable } from 'rxjs';
 
@@ -15,8 +15,16 @@ export class PensamentoService {
   listar(pagina: number): Observable<Pensamento[]> {
     
     const itensPorPagina = 6;
+
+    let params = new HttpParams()
+      .set("_page", pagina)
+      .set("_limit", itensPorPagina)
+
+    return this.http.get<Pensamento[]>(this.API, { params: params })
+    /*
     return this.http
     .get<Pensamento[]>(`${this.API}?_page=${pagina}&_limit=${itensPorPagina}`)
+    */
   }
 
   criar(pensamento: Pensamento): Observable<Pensamento> {
