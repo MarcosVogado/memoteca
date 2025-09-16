3179850ae6cca24c6bc2cabbeeeda81d2ff99776
Marcos Vogado <marcoshcvogado@gmail.com>
2025-09-16 15:32:42 -0300

    Add 'Carregar Mais' button to pensamentos list
    Introduces a 'Carregar Mais' button to the pensamentos list, allowing users to load more items. Updates the component, template, and styles to support this feature, and adds logic to handle pagination and display a message when there are no more items to load. Also updates the mock database with new sample pensamentos.

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -11,12 +11,22 @@ export class ListarPensamentosComponent {
 
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
 
   constructor(private service: PensamentoService){ }
 
   ngOnInit(): void {
     this.service.listar(this.paginaAtual).subscribe((listaPensamentos) =>{
       this.listaPensamentos = listaPensamentos
+    })
+  }
+
+  carregarMaisPensamentos(){
+    this.service.listar(++this.paginaAtual).subscribe(listaPensamentos => {
+      this.listaPensamentos.push(...listaPensamentos);
+      if(this.listaPensamentos.length) {
+        this.haMaisPensamentos = false
+      }
     })
   }
 
